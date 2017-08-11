18e8e47da7c39b993e3137b479b5af36cffce3ba
chengzhou <chengzhou@yit.com>
2017-08-11 21:57:51 +0800

    modify

---
+++ itechMobile/src/components/PositionItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="index">
+    <img src="" />
+    <div class="info">
+      <div>知名外企</div>
+      <div>互联专区</div>
+      <div>兼职</div>
+      <div>极速岗位</div>
+    </div>
+    <Tabbar selectedTab="首页"></Tabbar>
+  </div>
+</template>
+
+<script>
+import Tabbar from './Tabbar.vue'
+export default {
+  components: {Tabbar},
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img{
+  width:100%;
+  background:gray;
+}
+.info{
+  width:100%;
+}
+.info div{
+  width:21%;
+  margin:15px 2%;
+  float:left;
+  border:1px solid #000;
+  height:80px;
+  line-height:80px;
+  box-sizing:border-box;
+}
+
+</style>
---
+++ itechMobile/src/components/Tabbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="footer">
+    <mt-tabbar v-bind:fixed="true" :value="selectedTab">
+      <mt-tab-item id="首页">
+        <img slot="icon" src="../assets/home.png">
+        首页
+      </mt-tab-item>
+      <mt-tab-item id="职位">
+        <img slot="icon" src="../assets/search.png">
+        职位
+      </mt-tab-item>
+      <mt-tab-item id="公司">
+        <img slot="icon" src="../assets/company.png">
+        公司
+      </mt-tab-item>
+      <mt-tab-item id="我的">
+        <img slot="icon" src="../assets/me.png">
+        我的
+      </mt-tab-item>
+    </mt-tabbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  props: ['selectedTab'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
